20d1cafebf9bc6c7e53f0a9d52eb17d8ac8ba386
Piotrek <ryzykfizyk@gmail.com>
2018-09-29 19:34:29 +0200

    update jList

--- BooksCollection/src/BooksCollectionApp.java
+++ BooksCollection/src/BooksCollectionApp.java
@@ -22,8 +22,11 @@ public class BooksCollectionApp extends JFrame
 {
 	private JPanel panel = new JPanel();
 	private JTextField field = new JTextField(48);
-	private JButton button = new JButton("Add");
-	
+	private JButton button1 = new JButton("Add");
+	private JButton button2 = new JButton("Remove");
+	public static JList jList = null;
+	public static DefaultListModel listOfBooks = new DefaultListModel();
+	public static JScrollPane listOfBooksScrollPane;
 	public static ArrayList<String> list = new ArrayList<>();
 
 	public BooksCollectionApp()
@@ -32,79 +35,185 @@ public class BooksCollectionApp extends JFrame
 		panel.setPreferredSize(new Dimension(650, 450));
 		panel.setBackground(Color.GRAY);
 
-		final DefaultListModel listOfBooks = new DefaultListModel();
-
-		//listOfBooks.addElement("\"Unweaving the Rainbow\" | Richard Dawkins | Science, Delusion and the Appetite for Wonder");
-		for (String s : list)
-		{
-			listOfBooks.addElement(s);
-		}
-
-		JList jList = new JList(listOfBooks);
-		
-		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		jList.setPreferredSize(new Dimension(590, 100));
-		jList.setSelectedIndex(0);
-		jList.setVisibleRowCount(10);
-		
-		JScrollPane listOfBooksScrollPane = new JScrollPane(jList);
-		listOfBooksScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		listOfBooksScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		
-		getContentPane().add(listOfBooksScrollPane, BorderLayout.CENTER);
+		updateJList();
 
 		field.setText("Add Book in the following format: Title | Author | Description");
-		
-		ActionListener listener = new ActionListener()
-		{
-			public void actionPerformed(ActionEvent ee)
-			{
-				field.setText("");
-				field.requestFocus();
-			}
-		};
-		
+
 		MouseAdapter mouse = new MouseAdapter()
 		{
 			public void mousePressed(MouseEvent e)
 			{
-				if(field.getText().equals("Add Book in the following format: Title | Author | Description")) {
+				if (field.getText().equals("Add Book in the following format: Title | Author | Description"))
+				{
+					field.setText("");
+				}
+				if (field.getText().equals("Wrong data format!"))
+				{
 					field.setText("");
 				}
 			}
 		};
-		
-		ActionListener listenerButton = new ActionListener()
+
+		ActionListener listener = new ActionListener()
 		{
 			public void actionPerformed(ActionEvent ee)
 			{
-				field.setText("");
-				field.requestFocus();
+				if (field.getText().equals("Add Book in the following format: Title | Author | Description"))
+				{
+					field.setText("");
+				}
+				if (field.getText().equals("Wrong data format!"))
+				{
+					field.setText("");
+				}
+
+				String s = field.getText();
+				if (s.split("\\s\\|\\s").length == 3)
+				{
+					try
+					{
+						Database.insert(s);
+						Database.readList();
+						updateJList();
+
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+					field.setText("");
+					field.requestFocus();
+				} else
+				{
+					field.setText("Wrong data format!");
+					field.requestFocus();
+				}
+
+			}
+		};
+
+		ActionListener listenerButton1 = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ee)
+			{
+				if (field.getText().equals("Add Book in the following format: Title | Author | Description"))
+				{
+					field.setText("");
+				}
+				if (field.getText().equals("Wrong data format!"))
+				{
+					field.setText("");
+				}
+
+				String s = field.getText();
+				if (s.split("\\s\\|\\s").length == 3)
+				{
+					try
+					{
+						Database.insert(s);
+						Database.readList();
+						updateJList();
+
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+					field.setText("");
+					field.requestFocus();
+				} else
+				{
+					field.setText("Wrong data format!");
+					field.requestFocus();
+				}
+			}
+		};
+
+		ActionListener listenerButton2 = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ee)
+			{
+				int selectedIndex = jList.getSelectedIndex() + 1;
+				if (selectedIndex > 0)
+				{
+					try
+					{
+						Database.remove(selectedIndex);
+						Database.readList();
+						updateJList();
+						
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+				button2.requestFocus();
 			}
 		};
 
 		field.addMouseListener(mouse);
 		field.addActionListener(listener);
-		button.addActionListener(listenerButton);
-		
+		button1.addActionListener(listenerButton1);
+		button2.addActionListener(listenerButton2);
+
 		panel.add(listOfBooksScrollPane);
 		panel.add(field);
-		panel.add(button);
+		panel.add(button1);
+		panel.add(button2);
 		setContentPane(panel);
 		pack();
+	}
+
+	private void updateJList()
+	{
+		listOfBooks.clear();
+		for (String s : list)
+		{
+			listOfBooks.addElement(s);
+		}
+		
+		//jList = null;
+		//if(jList==null)
+			jList = new JList(listOfBooks);
+
+		//jList.removeAll();
+
+
+		//jList.setModel(listOfBooks);
+
+		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		//jList.setPreferredSize(new Dimension(590, 100));
+		jList.setFixedCellWidth(590);
+		//jList.setSelectedIndex(jList.getSelectedIndex());//TRICK!
+		//jList.setSelectedIndex(0);
+		jList.setVisibleRowCount(10);
+		
+		
+		jList.requestFocus();
+		
+		listOfBooksScrollPane = new JScrollPane(jList);
+		listOfBooksScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		listOfBooksScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		listOfBooksScrollPane.requestFocus();
+
+		getContentPane().add(listOfBooksScrollPane, BorderLayout.CENTER);
+		
+		//listOfBooksScrollPane.requestFocus();
+		//jList.clearSelection();
+		//jList.requestFocus();
+		
+		//System.out.println("jList.getSelectedIndex = " + jList.getSelectedIndex());
 	}
 
 	public static void main(String[] args) throws Exception
 	{
 		Database.create();
 		Database.readList();
-		Console.start(new BooksCollectionApp(), "Books Collection", 700, 500);		
-		
-		
-		for (String s : list)
-		{
-			System.out.println(s);
-		}
+		Console.start(new BooksCollectionApp(), "Collection of Books", 700, 270);
+
+//		for (String s : list)
+//		{
+//			System.out.println(s);
+//		}
 	}
 
 }
--- BooksCollection/src/Database.java
+++ BooksCollection/src/Database.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Database
@@ -48,14 +49,15 @@ public class Database
 	{
 		try
 		{
+			BooksCollectionApp.list.clear();
 			Connection con = getConnection();
 			PreparedStatement statement = con.prepareStatement("SELECT title, author, description FROM books");
 			ResultSet result = statement.executeQuery();
 
 			while (result.next())
 			{
-				BooksCollectionApp.list.add("\""+result.getString("title") + "\" | " + result.getString("author") + " | "
-						+ result.getString("description"));
+				BooksCollectionApp.list.add("\"" + result.getString("title") + "\" | " + result.getString("author")
+						+ " | " + result.getString("description"));
 			}
 
 			System.out.println("Selected all records!");
@@ -64,5 +66,51 @@ public class Database
 		{
 			System.out.println(e);
 		}
+	}
+
+	public static void insert(String s) throws Exception
+	{
+		String[] tab = s.split("\\s\\|\\s");
+
+		try
+		{
+			Connection con = getConnection();
+			PreparedStatement ps = con.prepareStatement("INSERT INTO books (title, author, description) VALUES ('"
+					+ tab[0] + "', '" + tab[1] + "','" + tab[2] + "')");
+			ps.executeUpdate();
+			con.close();
+		} catch (Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	public static void remove(int selectedIndex)
+	{
+		try
+		{
+			Connection con = getConnection();
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT ROW_NUMBER() OVER (ORDER BY id) row_num, id, title, author, description FROM books ORDER BY id;");
+			ResultSet result = ps.executeQuery();
+			String id = "";
+			int ident = 0;
+			while (result.next())
+			{
+				if (result.getInt("row_num") == selectedIndex)
+				{
+					ident = result.getInt("id");
+				}
+			}
+
+			PreparedStatement ps2 = con.prepareStatement("DELETE FROM books WHERE id = '" + ident + "'");
+			ps2.executeUpdate();
+			System.out.println("Deleted item: id = " + ident);
+			con.close();
+		} catch (Exception e)
+		{
+			System.out.println(e);
+		}
+
 	}
 }
